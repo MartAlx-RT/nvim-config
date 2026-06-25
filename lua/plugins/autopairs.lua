f28b04b41eb01019bb5c4ce70446a9bd71a5b0c9
MartAlx-RT <martynovalexvit99@gmail.com>
2026-06-25 22:20:59 +0300

    new autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,5 +1,31 @@
 return {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true -- автоматически запускает setup() с базовыми настройками
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  config = function()
+    -- 1. Initialization the plugin
+    local npairs = require("nvim-autopairs")
+    npairs.setup({})
+
+    -- 2. Paste the snippet directly here 
+    local Rule = require('nvim-autopairs.rule')
+    local cond = require('nvim-autopairs.conds')
+
+    npairs.add_rules({
+      Rule("\\left(", "\\right)", 'tex'),
+      Rule("\\left[", "\\right]", 'tex'),
+      Rule("\\left\\{", "\\right\\}", 'tex'),
+      Rule("\\left|", "\\right|", 'tex'),
+      Rule("\\left<", "\\right>", 'tex'),
+      Rule("$", "$", 'tex'),
+    })
+
+    local tex_brackets = { { '(', ')' }, { '[', ']' }, { '{', '}' } }
+    for _, bracket in pairs(tex_brackets) do
+      npairs.add_rules({
+        Rule(bracket[1], bracket[2], 'tex')
+          :with_pair(cond.not_after_regex([[%w]]))
+          :with_pair(cond.before_regex([[\\left$]] .. [[\|\\bigl$]] .. [[\|\\Bigl$]]))
+      })
+    end
+  end
 }
--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -6,6 +6,7 @@ return {
       -- Use VimTeX's default PDF viewer (e.g., 'zathura', 'sioyek', or 'view_mac')
       vim.g.vimtex_view_method = 'general'
       vim.g.vimtex_view_general_viewer = 'okular'
+
       -- Automatically open the quickfix window on compilation errors
       vim.g.vimtex_quickfix_mode = 0
     end
